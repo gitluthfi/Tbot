b84ebfed849e81045619c4dc4a8e5463de3a546e
gitluthfi <rafiluthfi247@gmail.com>
2023-08-24 13:41:00 +0700

    bug: error tidak muncul

--- Tbot.py
+++ Tbot.py
@@ -2,7 +2,8 @@ import os
 import ssl
 import subprocess
 import re
-import youtube_dl
+#import youtube_dl
+import yt_dlp as youtube_dl
 import asyncio
 
 import discord
@@ -64,6 +65,121 @@ async def on_ready():
 
 
 @client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+    #coba bot discord music#
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'aac',
+        'preferredquality': '192',
+    }],
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'source_address': '0.0.0.0'
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
+
+
+@bot.command(name='join', help='Tells the bot to join the voice channel')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+
+@bot.command(name='leave', help='To make the bot leave the voice channel')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@bot.command(name='play_song', help='To play song')
+async def play(ctx, url):
+    try:
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(url, loop=bot.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(
+                executable="ffmpeg.exe", source=filename))
+        await ctx.send('**Now playing:** {}'(filename))
+    except:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@bot.command(name='pause', help='This command pauses the song')
+async def pause(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.pause()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+
+
+@bot.command(name='resume', help='Resumes the song')
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        await voice_client.resume()
+    else:
+        await ctx.send("The bot was not playing anything before this. Use play_song command")
+
+
+@bot.command(name='stop', help='Stops the song')
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.stop()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+
+
+@client.event
 async def on_message(message):
     global voice_clients
     if message.author == client.user:
@@ -119,10 +235,20 @@ async def on_message(message):
             'format': 'bestaudio/best',
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
+                'preferredcodec': 'aac',
                 'preferredquality': '192',
             }],
-            'verbose': True,  # check certficate
+            'verbose': True,  # check certificate
+            'nocheckcertificate': True,  # Disable SSL certificate verification
+            'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+            'restrictfilenames': True,
+            'noplaylist': True,
+            'ignoreerrors': False,
+            'logtostderr': False,
+            'quiet': True,
+            'no_warnings': True,
+            'default_search': 'auto',
+            'source_address': '0.0.0.0'
         }
 
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -130,14 +256,15 @@ async def on_message(message):
                 info = ydl.extract_info(url, download=False)
                 url2 = info['formats'][0]['url']
                 voice_client.play(discord.FFmpegPCMAudio(url2))
+
+                # Menunggu hingga lagu selesai sebelum keluar dari channel suara
+                while voice_client.is_playing():
+                    await asyncio.sleep(1)
+
+                # Setelah lagu selesai, disconnect dari channel suara
+                await voice_client.disconnect()
             except Exception as e:
                 print(f"An error occurred during music playback: {e}")
-
-        # Menunggu hingga lagu selesai sebelum keluar dari channel suara
-        await voice_client.is_playing()
-        # await asyncio.sleep(1)
-
-        # await voice_client.disconnect()
     elif message.content.startswith('!show_databases'):
         cmd = f"mysql -h {DB_HOST} -u {DB_USER} -e 'show databases;'"
         try:
@@ -200,6 +327,9 @@ async def on_message(message):
         except Exception as e:
             # Handle other exceptions gracefully
             await message.channel.send(f"An error occurred: {e}")
+    elif message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
 
         # Print the executed command for debugging purposes
         # print(cmd)
