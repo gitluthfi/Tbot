b9e904362db8133e9aa5544739dbbe7f9e33baf3
gitluthfi <rafiluthfi247@gmail.com>
2023-08-25 13:38:29 +0700

    fix and add : music feature, proto, run

--- Tbot.py
+++ Tbot.py
@@ -1,10 +1,14 @@
+# fix uploader id error
+# watch this bro, this unreal
+
 import os
 import ssl
 import subprocess
 import re
 import youtube_dl
 import asyncio
-
+import pandas as pd
+import io
 import discord
 import aiohttp
 from dotenv import load_dotenv
@@ -26,7 +30,7 @@ DB_USER = os.getenv('DB_USERNAME')
 DB_PW = os.getenv('DB_PASSWORD')
 intents = discord.Intents.default()
 intents.members = True
-bot = commands.Bot(command_prefix='!raib', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 sslcontext = ssl.create_default_context()
 sslcontext.check_hostname = False
 sslcontext.verify_mode = ssl.CERT_NONE
@@ -58,25 +62,132 @@ async def on_ready():
 
         # INCREMENTS THE GUILD COUNTER.
         guild_count = guild_count + 1
+        # Check if there is a channel named "general" in the guild
+        general_channel = discord.utils.get(
+            guild.text_channels, name="general")
 
-    # PRINTS HOW MANY GUILDS / SERVERS THE BOT IS IN.
-    print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
+        if general_channel:
+            # Send a message in the "general" channel
+            if general_channel.permissions_for(guild.me).send_messages:
+                await general_channel.send("@everyone ILY ready to serve!")
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'source_address': '0.0.0.0'
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
+
+
+@bot.command(name='join', help='Tells the bot to join the voice channel')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+
+@bot.command(name='leave', help='To make the bot leave the voice channel')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@bot.command(name='play_song', help='To play song')
+async def play(ctx, url):
+    try:
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(url, loop=bot.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(
+                executable="ffmpeg.exe", source=filename))
+        await ctx.send('**Now playing:** {}'.format(filename))
+    except:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@bot.command(name='pause', help='This command pauses the song')
+async def pause(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.pause()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+
+
+@bot.command(name='resume', help='Resumes the song')
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        await voice_client.resume()
+    else:
+        await ctx.send("The bot was not playing anything before this. Use play_song command")
+
+
+@bot.command(name='stop', help='Stops the song')
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.stop()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
 
 
 @client.event
 async def on_message(message):
     global voice_clients
+    perintah = '!ILY'
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
+    if message.content.startswith(f"{perintah} hello"):
         await message.channel.send("Hello")
-    elif message.content.startswith('berikan aku ruang'):
+    elif message.content.startswith(f"{perintah} berikan aku ruang"):
         # Mengambil server (guild) tempat pesan tersebut diposting
         guild = message.guild
         parts = message.content.split(" ")
-        if len(parts) >= 4:
-            namespace = " ".join(parts[3:])
+        if len(parts) >= 5:
+            namespace = " ".join(parts[4:])
 
     # Membuat saluran teks baru dengan nama "ruang-<nama_pengguna>"
         channel_name = f"ruang-{namespace}"
@@ -87,7 +198,7 @@ async def on_message(message):
     #                               #
     #INI MASIH DALAM TAHAP UJI COBA #
     #                               #
-    elif message.content.startswith('aku mau ngomong sesuatu ra'):
+    elif message.content.startswith(f"{perintah} aku mau ngomong sesuatu ra"):
         guild = message.guild
 
     # Membuat kategori (category) untuk thread baru
@@ -104,9 +215,9 @@ async def on_message(message):
     elif message.content.startswith('hai, ra!'):
         await message.channel.send('hai, ali')
 
-    elif message.content.startswith('play'):
+    elif message.content.startswith(f"{perintah} play"):
         # Mendapatkan URL lagu dari pesan pengguna
-        url = message.content[len('play'):].strip()
+        url = message.content[len(f"{perintah} play"):].strip()
 
         if message.author.voice is None:
             await message.channel.send("Anda harus bergabung dengan channel suara terlebih dahulu.")
@@ -117,12 +228,26 @@ async def on_message(message):
 
         ydl_opts = {
             'format': 'bestaudio/best',
+            'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
+                'preferredcodec': 'opus',
+                'preferredquality': '256',
             }],
-            'verbose': True,  # check certficate
+            'restrictfilenames': True,
+            'noplaylist': True,
+            'nocheckcertificate': True,
+            'ignoreerrors': False,
+            'logtostderr': False,
+            'quiet': True,
+            'no_warnings': True,
+            'default_search': 'auto',
+            # bind to ipv4 since ipv6 addresses cause issues sometimes
+            'source_address': '0.0.0.0',
+        }
+
+        ffmpeg_options = {
+            'options': '-vn -b:a 256k -af loudnorm',
         }
 
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -130,16 +255,29 @@ async def on_message(message):
                 info = ydl.extract_info(url, download=False)
                 url2 = info['formats'][0]['url']
                 voice_client.play(discord.FFmpegPCMAudio(url2))
+                await message.channel.send('Lagu anda diputar, ENJOY!!!')
+                print("Playing music...")  # Add this line for debugging
+
+                while voice_client.is_playing():
+                    await asyncio.sleep(1)
+
+                print("Finished playing.")  # Add this line for debugging
+                await voice_client.disconnect()
             except Exception as e:
                 print(f"An error occurred during music playback: {e}")
+    elif message.content.startswith(f"{perintah} show_databases"):
+        cmd = f"mysql -h {DB_HOST} -u {DB_USER} -e 'select DB_ILY.m_staging.database_name, DB_ILY.m_staging.project_name from DB_ILY.m_staging;'"
+        # parts = message.content.split(" ")
+        # if len(parts) >= 1:
+        #     link = " ".join(parts[1:])
+        #     cmd = f"mysql -h {DB_HOST} -u {DB_USER} -e 'select DB_ILY.m_staging.database_name, DB_ILY.m_staging.project_name from DB_ILY.m_staging;"
 
-        # Menunggu hingga lagu selesai sebelum keluar dari channel suara
-        await voice_client.is_playing()
-        # await asyncio.sleep(1)
-
-        # await voice_client.disconnect()
-    elif message.content.startswith('!show_databases'):
-        cmd = f"mysql -h {DB_HOST} -u {DB_USER} -e 'show databases;'"
+        if message.content.startswith(f"{perintah} show_databases"):
+            parts = message.content.split(" ")
+            if len(parts) >= 2:
+                link = " ".join(parts[2:])
+                cmd = f"mysql -h {DB_HOST} -u {DB_USER} -e 'select DB_ILY.m_staging.database_name, DB_ILY.m_staging.project_name from DB_ILY.m_staging where project_name like \"%{link}%\";'"
+                # print(cmd)
         try:
             result = subprocess.run(
                 cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
@@ -151,8 +289,8 @@ async def on_message(message):
             output = f"An error occurred: {e}"
 
         await message.channel.send(output)
-    elif message.content.startswith('!backup'):
-        database_name = message.content.split(" ")[1]
+    elif message.content.startswith(f"{perintah}backup"):
+        database_name = message.content.split(" ")[2]
         path_file = f"/Users/rafiizzatulrizqufaris/Documents/python_upi/{database_name}.sql"
         cmd = f"mysqldump -h {DB_HOST} -u {DB_USER} {database_name} > {path_file}"
         try:
@@ -171,16 +309,16 @@ async def on_message(message):
         except subprocess.CalledProcessError as e:
             # If the command returns a non-zero exit code, there was an error
             await message.channel.send(f"Error during database backup:\n```{e.stderr}```")
-    elif message.content.startswith('!run'):
+    elif message.content.startswith(f"{perintah}run"):
         # Split the message content into parts
         parts = message.content.split(" ")
 
     # Check if there are at least 3 parts (command, database name, and user query)
-        if len(parts) >= 3:
+        if len(parts) >= 4:
             # Extract the database name and user query
-            database_name = parts[1]
+            database_name = parts[2]
         # Join the remaining parts to form the user query
-            user_query = " ".join(parts[2:])
+            user_query = " ".join(parts[3:])
 
         # Construct the MySQL command with proper quoting
             cmd = f"mysql -h {DB_HOST} -u {DB_USER} -e \"{user_query}\" {database_name}"
@@ -203,6 +341,48 @@ async def on_message(message):
 
         # Print the executed command for debugging purposes
         # print(cmd)
+    elif message.content.startswith(f"{perintah}proto"):
+        # Split the message content into parts
+        parts = message.content.split(" ")
+
+    # Check if there are at least 3 parts (command, database name, and user query)
+        if len(parts) >= 4:
+            # Extract the database name and user query
+            database_name = parts[2]
+        # Join the remaining parts to form the user query
+            user_query = " ".join(parts[3:])
+
+        # Construct the MySQL command with proper quoting
+        cmd = f"mysql -h {DB_HOST} -u {DB_USER} -e \"{user_query}\" {database_name}"
+
+        try:
+            # Run the query command
+            result = subprocess.run(
+                cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+            if result.returncode == 0:
+                # Assuming the query result is in a tabular format, you can use pandas to read it
+                query_result = pd.read_csv(
+                    io.StringIO(result.stdout), delimiter="\t")
+
+                # Save the query result to a CSV file
+                csv_filename = f"{database_name}_query_result.csv"
+                query_result.to_csv(csv_filename, index=False)
+
+                # Send the CSV file
+                with open(csv_filename, 'rb') as file:
+                    await message.channel.send(f"Query result for database '{database_name}':", file=discord.File(file, csv_filename))
+
+                # Delete the temporary CSV file
+                os.remove(csv_filename)
+
+            else:
+                # If there's an error, send an error message
+                await message.channel.send(f"Error executing the query:\n```{cmd}```")
+
+        except Exception as e:
+            # Handle other exceptions gracefully
+            await message.channel.send(f"An error occurred: {e}")
 
 
 @bot.event
