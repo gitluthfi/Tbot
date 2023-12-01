c6212d50cea0aca26f6d2bf778d940853418518f
gitluthfi <rafiluthfi247@gmail.com>
2023-12-01 10:40:29 +0700

    refactor: reply messages & fix proto

--- Tbot.py
+++ Tbot.py
@@ -45,7 +45,7 @@ PATH_FILE= os.getenv('PATH_FILE')
 ENV= os.getenv('BOT_ENV')
 FOLDER_ID = os.getenv("FOLDER_DRIVE")
 GENIUS_API = os.getenv('GENIUS_API_ID')
-#docker_client = docker.from_env()
+docker_client = docker.from_env()
 genius = lyricsgenius.Genius(GENIUS_API)
 intents = discord.Intents.default()
 intents.members = True
@@ -129,10 +129,10 @@ async def on_message(message):
                     # got the real url from extracted url
                     url2 = info['formats'][0]['url']
                 except Exception as e:
-                    await message.channel.send(f"An error occurred while fetching song: {e}")
+                    await message.reply(f"An error occurred while fetching song: {e}")
                 # play the song
                 voice_client.play(discord.FFmpegPCMAudio(url2))
-                await message.channel.send(f"Lagu anda diputar, ENJOY!!! \n {video_url}")
+                await message.reply(f"Lagu anda diputar, ENJOY!!! \n {video_url}")
                 print("Playing music...")  # Add this line for debugging
 
 
@@ -147,12 +147,12 @@ async def on_message(message):
                 with open(file_name, "w", encoding="utf-8") as lyrics_file:
                     lyrics_file.write(lyrics)
                 with open(file_name, "rb") as lyrics_file:
-                    await message.channel.send(f"**Lyrics for {song_title}:**", file=discord.File(lyrics_file, file_name))
+                    await message.reply(f"**Lyrics for {song_title}:**", file=discord.File(lyrics_file, file_name))
                 os.remove(file_name)
             else:
-                await message.channel.send("Lirik tidak ditemukan.")
+                await message.reply("Lirik tidak ditemukan.")
         except Exception as e:
-            await message.channel.send(f"An error occurred while fetching lyrics: {e}")
+            await message.reply(f"An error occurred while fetching lyrics: {e}")
 
     async def parse_lagu(url):
         videosSearch = VideosSearch(url, limit=1)
@@ -160,7 +160,7 @@ async def on_message(message):
         results = videosSearch.result()
         video_url = results['result'][0]['link']
         if not results:
-            await message.channel.send("Lagu tidak ditemukan")
+            await message.reply("Lagu tidak ditemukan")
             return
         return video_url
     
@@ -172,24 +172,24 @@ async def on_message(message):
                 if voice_client.is_playing():
                     voice_client.pause()
                     print('music paused')
-                    await message.channel.send("Music paused.")
+                    await message.reply("Music paused.")
                 else:
-                    await message.channel.send("There is no music playing to pause.")
+                    await message.reply("There is no music playing to pause.")
             
             elif message.content.startswith(f"{perintah} start"):
                 if voice_client.is_paused():
                     voice_client.resume()
                     print('music resumed')
-                    await message.channel.send("Music resumed.")
+                    await message.reply("Music resumed.")
                 else:
-                    await message.channel.send("Music is not paused.")
+                    await message.reply("Music is not paused.")
                 
             elif message.content.startswith(f"{perintah} stop"):
                 if voice_client.is_playing():
                     voice_client.stop()
-                    await message.channel.send("Music stopped.")
+                    await message.reply("Music stopped.")
                 else:
-                    await message.channel.send("There is no music playing to stop.")
+                    await message.reply("There is no music playing to stop.")
             
             elif message.content.startswith(f"{perintah} skip"):
                 await skip_song(message)
@@ -198,7 +198,7 @@ async def on_message(message):
                 queue_url = message.content[len(f"{perintah} add"):].strip()
                 queue_video_url = await parse_lagu(queue_url)
                 queue.append(queue_video_url)
-                await message.channel.send(f"Musik masuk ke antrian {queue_video_url}")
+                await message.reply(f"Musik masuk ke antrian {queue_video_url}")
         
         except asyncio.TimeoutError:
             pass
@@ -222,10 +222,10 @@ async def on_message(message):
         
         if voice_client.is_playing():
             voice_client.stop()
-            await message.channel.send("Song skipped.")
+            await message.reply("Song skipped.")
             await play_next_song(message)
         else:
-            await message.channel.send("There is no song playing to skip.")
+            await message.reply("There is no song playing to skip.")
     if (ENV == "production"):
         perintah = '!ILY'
     elif (ENV == "develop"):
@@ -234,7 +234,7 @@ async def on_message(message):
         return
 
     if message.content.startswith(f"{perintah} hello"):
-        await message.channel.send("Hello")
+        await message.reply("Hello")
     elif message.content.startswith(f"{perintah} berikan aku ruang"):
         # Mengambil server (guild) tempat pesan tersebut diposting
         guild = message.guild
@@ -264,14 +264,14 @@ async def on_message(message):
         await thread.send(f'kemari ali! {message.author.mention}')
     # batas uji coba
     elif message.content.startswith('!HAI'):
-        await message.channel.send(f'hai, {message.author.mention}')
+        await message.reply(f'hai, {message.author.mention}')
     elif message.content.startswith('hai, ra!'):
-        await message.channel.send('hai, ali')
+        await message.reply('hai, ali')
 
     elif message.content.startswith(f"{perintah} play"):
         #connect ke voice channel
         if message.author.voice is None:
-            await message.channel.send("Anda harus bergabung dengan channel suara terlebih dahulu.")
+            await message.reply("Anda harus bergabung dengan channel suara terlebih dahulu.")
             return
 
         voice_channel = message.author.voice.channel
@@ -281,7 +281,7 @@ async def on_message(message):
         url = message.content[len(f"{perintah} play"):].strip()
         video_url = await parse_lagu(url)
         #Ambil link lagu dari data pencarian
-        await message.channel.send('Tunggu sebentar pesanan anda sedang di proses')
+        await message.reply('Tunggu sebentar pesanan anda sedang di proses')
         await search_lyric(url)
         await play_song(video_url)
         while voice_client.is_playing() or voice_client.is_paused():
@@ -317,11 +317,11 @@ async def on_message(message):
         except Exception as e:
             output = f"An error occurred: {e}"
 
-        await message.channel.send(output)
+        await message.reply(output)
     elif message.content.startswith(f"{perintah} backup"):
         database_name = message.content.split(" ")[2]
         cmd = f"mysqldump -h {DB_HOST} -u {DB_USER} -p{DB_PW} {database_name} > {PATH_FILE}/{database_name}.sql"
-        await message.channel.send(f"Mohon tunggu sebentar pesanan mu sedang di proses {message.author.mention}")
+        await message.reply(f"Mohon tunggu sebentar pesanan mu sedang di proses {message.author.mention}")
         try:
             # Run the backup command
             result = subprocess.run(cmd, shell=True, text=True, check=True)
@@ -344,16 +344,16 @@ async def on_message(message):
             file_link = file['alternateLink']
 
             # Send a success message
-            await message.channel.send(f"Sir {message.author.mention} \n Database backup completed successfully. \n Here {file_link}")
+            await message.reply(f"Sir {message.author.mention} \n Database backup completed successfully. \n Here {file_link}")
 
             # Send the backup file to the same channel
             backup_file = discord.File(f"{PATH_FILE}/{database_name}.sql")
-            # await message.channel.send(file=backup_file)
+            # await message.reply(file=backup_file)
             os.remove(f"{PATH_FILE}/{database_name}.sql")
 
         except subprocess.CalledProcessError as e:
             # If the command returns a non-zero exit code, there was an error
-            await message.channel.send(f"Error during database backup:\n```{e.stderr}```")
+            await message.reply(f"Error during database backup:\n```{e.stderr}```")
 
     elif message.content.startswith(f"{perintah} run"):
         # Split the message content into parts
@@ -368,6 +368,8 @@ async def on_message(message):
 
         # Construct the MySQL command with proper quoting
             cmd = f"mysql -h {DB_HOST} -u {DB_USER} -p{DB_PW} -e \"{user_query}\" {database_name}"
+        else:
+            await message.reply(f"Sir {message.author.mention}, perintah tidak lengkap mohon lenkapi seperti ini \n `!ILY run database_name your_query`")
 
         try:
             # Run the query command
@@ -376,18 +378,18 @@ async def on_message(message):
 
             if result.returncode == 0:
                 # Send the query result as a message
-                await message.channel.send(f"Query result for database '{database_name}':\n```{result.stdout}```")
+                await message.reply(f"Query result for database '{database_name}':\n```{result.stdout}```")
             else:
                 # If there's an error, send an error message
-                await message.channel.send(f"Error executing the query:\n```{cmd}```")
+                await message.reply(f"Error executing the query:\n```{cmd}```")
 
         except Exception as e:
             # Handle other exceptions gracefully
-            await message.channel.send(f"An error occurred: {e}")
+            await message.reply(f"An error occurred: {e}")
 
         # Print the executed command for debugging purposes
         # print(cmd)
-    elif message.content.startswith(f"{perintah} proto"):
+    elif message.content.startswith(f"{perintah} get"):
         # Split the message content into parts
         parts = message.content.split(" ")
 
@@ -417,32 +419,37 @@ async def on_message(message):
 
                 # Send the CSV file
                 with open(csv_filename, 'rb') as file:
-                    await message.channel.send(f"Query result for database '{database_name}':", file=discord.File(file, csv_filename))
+                    await message.reply(f"Query result for database '{database_name}':", file=discord.File(file, csv_filename))
 
                 # Delete the temporary CSV file
                 os.remove(csv_filename)
 
             else:
                 # If there's an error, send an error message
-                await message.channel.send(f"Error executing the query:\n```{cmd}```")
+                await message.reply(f"Error executing the query:\n```{cmd}```")
 
         except Exception as e:
             # Handle other exceptions gracefully
-            await message.channel.send(f"An error occurred: {e}")
+            await message.reply(f"An error occurred: {e}")
     elif message.content.startswith(f"{perintah} deploy"):
 
+        if len(message.content.split(" ")) == 4:
+
         # parse
-        project_name = message.content.split(" ")[2]
-        environment = message.content.split(" ")[3]        
+            project_name = message.content.split(" ")[2]
+            environment = message.content.split(" ")[3]        
 
-        if (environment == "staging"):
-           tag = "latest"
+            if (environment == "staging"):
+                tag = "latest"
 
-        print(f"Project Name: {project_name}")
-        print(f"Tag: {tag}")
-        print(f"Environment: {environment}")
+            print(f"Project Name: {project_name}")
+            print(f"Tag: {tag}")
+            print(f"Environment: {environment}")
 
-        await message.channel.send(f"Mohon tunggu sir {message.author.mention}, pesanan anda sedang di proses")
+            await message.reply(f"Mohon tunggu sir {message.author.mention}, pesanan anda sedang di proses")
+        else:
+            await message.reply(f"Sir {message.author.mention}, perintah tidak lengkap mohon lenkapi seperti ini \n `!ILY deploy project_name environment`")    
+            return
 
         # Check if a container with the specified project name and tag exists
         container_name = f"{project_name}-{environment}"
@@ -469,7 +476,7 @@ async def on_message(message):
         
         if existing_image:
             docker_client.images.remove(project_name)
-        await message.channel.send(f"Sir {message.author.mention}, Pesananmu sedang dalam proses build, tunggu sebentar ya!! \n Sambil menunggu proses build, nikmati layanan musik saya, dapat diakses dengan command {perintah} play judul_lagu_anda")
+        await message.reply(f"Sir {message.author.mention}, Pesananmu sedang dalam proses build, tunggu sebentar ya!! \n Sambil menunggu proses build, nikmati layanan musik saya, dapat diakses dengan command {perintah} play judul_lagu_anda")
         #build docker file
         if (project_name == ('sitrendy')):
             build_path = '/var/lib/jenkins/workspace/sitrendy'
@@ -478,16 +485,20 @@ async def on_message(message):
                 tag=image_name,
                 rm=True
             )
+        else:
+            await message.reply(f"Sir {message.author.mention} project tidak ditemukan, segera hubungi admin")
         # Run a new container with port mapping and volume attachment
         if (project_name == ('sitrendy')):
             container = docker_client.containers.run(
                 image=image_name,
                 detach=True,
                 ports={'80/tcp': 1010},
-                volumes={'/var/lib/jenkins/workspace/sitrendy': {'bind': '/var/www/html/app', 'mode': 'rw'},},
+                volumes={build_path: {'bind': '/var/www/html/app', 'mode': 'rw'},},
                 name=container_name
             )
-        await message.channel.send(f"{project_name} berhasil di deploy sir, {message.author.mention} \n Container ID: {container.id} \n Container Name: {container.name} \n Container Image: {container.image.tags[0]}")
+        else:
+            await message.reply(f"Sir {message.author.mention} project tidak ditemukan, segera hubungi admin")
+        await message.reply(f"{project_name} berhasil di deploy sir, {message.author.mention} \n Container ID: {container.id} \n Container Name: {container.name} \n Container Image: {container.image.tags[0]}")
 
 @bot.event
 async def on_message(message):
--- tempat.py
+++ tempat.py
@@ -1,6 +1,2 @@
-from collections import deque
-
-queue = deque(['luthfi', 'duar', 'nmax', 'bebek'])
-
-for data in queue:
-    print(data)
+if (project_name):
+    switch
